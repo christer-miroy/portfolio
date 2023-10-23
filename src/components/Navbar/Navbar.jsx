1359950fbc68e3e7586dc7ac2dbc5f7ac5c1ce36
Christer Miroy <chrstrzm.mry@gmail.com>
2023-10-23 23:30:04 +0800

    edit navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
   return <nav className={styles.navbar}>
-    <a className={styles.title} href="/">Portfolio</a>
+    <a className={styles.title} href="#">Portfolio</a>
     <div className={styles.menu}>
         {/* responsive menu button */}
         <img
